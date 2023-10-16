5b7bc09e0d433895cbd83fbfcbac1e5f52a8ff2f
Moonkis <victor.karlsson.github@gmail.com>
2023-10-16 19:15:17 +0200

    Added interface to remove dependency on HtmlAgilityPack

---
+++ octo/IHtmlPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace octo
+{
+    internal interface IHtmlPage
+    {
+        public List<PageInformation> GetChildPages();
+        public List<PageInformation> GetStaticResources();
+        public string Load(Uri uri);
+        public bool Save(string filePath);
+    }
+}
