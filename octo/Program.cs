990b3a68580c0c16dba4d0f9ca8ca4c05f0cd321
Moonkis <victor.karlsson.github@gmail.com>
2023-10-16 17:35:27 +0200

    Created small PoC skeleton

--- octo/Program.cs
+++ octo/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace octo
+﻿using System.Configuration;
+
+namespace octo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var crawler = new WebCrawler(new CrawlerSettings() 
+            { 
+                OutputDirectory = ConfigurationManager.AppSettings["OutputDirectory"] ?? string.Empty
+            });
+
+            crawler.Crawl(new Uri(ConfigurationManager.AppSettings["Url"] ?? string.Empty));
         }
     }
 }
---
+++ octo/WebCrawler.cs
@@ -0,0 +1,59 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace octo
+{
+    struct CrawlerSettings
+    {
+        public string OutputDirectory { get; set; }
+    }
+
+    internal class WebCrawler
+    {
+        private CrawlerSettings m_Settings;
+
+        public WebCrawler(CrawlerSettings settings) 
+        {
+            if (string.IsNullOrEmpty(settings.OutputDirectory))
+            {
+                _ = settings.OutputDirectory ?? throw new ArgumentNullException(nameof(settings.OutputDirectory));
+                throw new ArgumentException($"{nameof(settings.OutputDirectory)} can't be empty.");
+            }
+
+            m_Settings = settings;
+        }
+
+        public void Crawl(Uri uri)
+        {
+            if (!Uri.IsWellFormedUriString(uri.OriginalString, UriKind.Absolute))
+            {
+                throw new ArgumentException($"{nameof(uri)} is incorrectly formed.");
+            }
+
+            HtmlWeb html = new HtmlWeb();
+            var document = html.Load(uri);
+
+            var localPath = GetLocalPath(uri);
+            var extension = Path.GetExtension(localPath);
+
+            if (string.IsNullOrEmpty(extension))
+            {
+                localPath = Path.Combine(localPath, "index.html");
+            }
+
+            Directory.CreateDirectory(Path.GetDirectoryName(localPath));
+            document.Save(localPath);
+        }
+
+        private string GetLocalPath(Uri uri)
+        {
+            return $"{m_Settings.OutputDirectory}/{uri.Host}{uri.AbsolutePath}";
+        }
+    }
+}
